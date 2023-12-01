574fc2e3436a39cc82d369c8d267bd53d6938113
Miehau <michal.mlak.1@gmail.com>
2023-12-01 20:03:06 +0000

    Day 1: Trebuchet?

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,61 @@
+private val wordToDigit = buildMap {
+    put("one", "1")
+    put("two", "2")
+    put("three", "3")
+    put("four", "4")
+    put("five", "5")
+    put("six", "6")
+    put("seven", "7")
+    put("eight", "8")
+    put("nine", "9")
+}
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { line -> extractNumber(line) }
+            .sumOf { it.toInt() }
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return part1(
+            input.map { line -> extractNumbersFrom(line) }
+        )
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("day1")
     part1(input).println()
     part2(input).println()
+}
+
+fun extractNumber(line: String): String {
+    val first = line.find { it.isDigit() }
+    val last = line.findLast { it.isDigit() }
+    return first.toStringOrEmpty() + last.toStringOrEmpty()
+}
+
+private fun Char?.toStringOrEmpty(): String {
+    return this?.toString() ?: ""
+}
+
+fun extractNumbersFrom(input: String): String {
+    return input.mapIndexed() { index, char ->
+        if (char.isDigit()) {
+            char.toString()
+        } else {
+            val substringFromIndex = input.substring(index)
+            getDigitFromBeginningOfWord(substringFromIndex)
+        }
+    }.joinToString("")
+}
+
+private fun getDigitFromBeginningOfWord(input: String): String {
+    wordToDigit.forEach { entry ->
+        if (input.startsWith(entry.key)) {
+            return entry.value
+        }
+    }
+    return ""
 }
